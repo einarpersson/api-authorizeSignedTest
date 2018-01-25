f17caf3e229a2cccd8b54481848431b86bd5ed19
martinloman <martin.loman@gmail.com>
2018-01-25 09:05:28 +0100

    Added code to test GET requests.

--- index.js
+++ index.js
@@ -5,25 +5,52 @@ const url = '<URL>'
 const tokenId = '<TOKENID>'
 const tokenSecret = '<TOKENSECRET>'
 
+
+
+/******** FOR GET REQUEST  ********/
+const timestamp = new Date().toISOString()
+const urlToGet = `${url}/stats/calls/mostcommon?phonenumber=0707227024&noofPhonenumbers=5&tokenId=${tokenId}`
+const signatureDataGet = urlToGet + ';' + timestamp
+
+const calculatedHashGet = crypto.createHmac('sha512', tokenSecret).update(signatureDataGet, 'utf8').digest('base64')
+
+const optionsGet = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Date': timestamp,
+        'digest': `HMAC-SHA512=${calculatedHashGet}`
+    }
+}
+
+fetch(urlToGet, optionsGet)
+    .then(res => {
+        return res.json()
+    })
+    .then(parsedRes => console.log('GET SUCCESS', parsedRes))
+    .catch(err => console.log('GET ERROR', err))
+
+
+/******** FOR POST REQUEST  ********/
 const payload = {
-  tokenId: tokenId,
-  timestamp: new Date().toISOString(),
-  checkout: null,
+    tokenId: tokenId,
+    timestamp: new Date().toISOString(),
+    checkout: null,
+}
+const signatureDataPost = JSON.stringify(payload)
+const calculatedHashPost = crypto.createHmac('sha512', tokenSecret).update(signatureDataPost, 'utf8').digest('base64')
+const optionsPost = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'digest': `HMAC-SHA512=${calculatedHashPost}`
+    },
+    body: JSON.stringify(payload)
 }
 
-const calculatedHash = crypto.createHmac('sha512', tokenSecret).update(JSON.stringify(payload), 'utf8').digest('base64')
-
-const options = {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'digest': `HMAC-SHA512=${calculatedHash}`
-  },
-  body: JSON.stringify(payload)
-}
-
-fetch(`${url}/orders`, options)
-  .then(res => res.json())
-  .then(parsedRes => console.log('SUCCESS', parsedRes))
-  .catch(err => console.log('ERROR', err))
+fetch(`${url}/orders`, optionsPost)
+    .then(res => res.json())
+    .then(parsedRes => console.log('POST SUCCESS', parsedRes))
+    .catch(err => console.log('POST ERROR', err))
